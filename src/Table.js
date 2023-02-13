56d6103a73183a15e79135f966872f35b268332e
rbverissimo <rverissimo.dev@gmail.com>
2023-02-13 13:59:05 -0300

    Solving some bugs: Table.js component must have a return.

--- src/Table.js
+++ src/Table.js
@@ -2,32 +2,34 @@ import React, { Component } from "react";
 
 class Table extends Component {
   render() {
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Dancer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring Actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Charlie</td>
+            <td>Janitor</td>
+          </tr>
+          <tr>
+            <td>Mac</td>
+            <td>Dancer</td>
+          </tr>
+          <tr>
+            <td>Dee</td>
+            <td>Aspiring Actress</td>
+          </tr>
+          <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 }
 
