f184f5a533e8cb6448fd57766ce8ba9ef9fb6e68
rbverissimo <rverissimo.dev@gmail.com>
2023-02-13 13:55:05 -0300

    Implementing a custom Table component in react and passing it to the App.js to be rendered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Table from "./Table";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table />
       </div>
     );
   }
 }
+
+export default App;
---
+++ src/Table.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Table extends Component {
+  render() {
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Dancer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring Actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </tbody>
+    </table>;
+  }
+}
+
+export default Table;
