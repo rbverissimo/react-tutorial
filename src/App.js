beca78b677f06ab7557109174cbca144473a6571
rbverissimo <rverissimo.dev@gmail.com>
2023-02-15 10:48:31 -0300

    Starting state implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from "react";
 import Table from "./Table";
 
 class App extends Component {
+  state = {
+    characters: [
+      {
+        name: "Charlie",
+        job: "Janitor",
+      },
+      {
+        name: "Mac",
+        job: "Bouncer",
+      },
+      {
+        name: "Dee",
+        job: "Aspiring Actress",
+      },
+      {
+        name: "Dennis",
+        job: "Bartender",
+      },
+    ],
+  };
   render() {
     const characters = [
       {
