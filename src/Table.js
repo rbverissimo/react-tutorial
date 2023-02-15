e3d0f4ce1c5b8de15eb21e7b4ec877aa315b916b
rbverissimo <rverissimo.dev@gmail.com>
2023-02-15 10:09:29 -0300

    Transforming the React Component into ES6 arrow functions for both body and header of the table

--- src/Table.js
+++ src/Table.js
@@ -1,36 +1,35 @@
 import React, { Component } from "react";
 
-class Table extends Component {
-  render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Dancer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring Actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-}
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+};
 
-export default Table;
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Dancer</td>
+      </tr>
+      <tr>
+        <td>Dee</td>
+        <td>Aspiring Actress</td>
+      </tr>
+      <tr>
+        <td>Dennis</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  );
+};
