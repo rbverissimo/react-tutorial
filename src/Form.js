a31faafbbb3e765eee43321e8e445d5e5fe6e61c
rbverissimo <rverissimo.dev@gmail.com>
2023-02-15 22:44:51 -0300

    A little refactoring on App.js. Also, implemented the whole logic of the Form class Component with the handleChange method that basically alters the state for us

--- src/Form.js
+++ src/Form.js
@@ -7,4 +7,40 @@ class Form extends Component {
   };
 
   state = this.initialState;
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { name, job } = this.state;
+    return (
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="job">Job</label>
+        <input
+          type="text"
+          name="job"
+          id="job"
+          value={job}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
 }
+
+export default Form;
+
+//on older React versions it was necessary to include a constructor() on Components
